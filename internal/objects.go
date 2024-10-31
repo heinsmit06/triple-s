ba2fff22bc56fc17429c21856983789ab1c0f041
myertaye <medetyertayev@gmail.com>
2024-10-31 22:36:51 +0500

    Added a function to check if there is such directory when PUTting a new file inside buckets

--- internal/objects.go
+++ internal/objects.go
@@ -1,8 +1,35 @@
 package internal
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func CreateObjects(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path[1:]
+	pathSlice := strings.Split(path, "/")
+	bucketName := pathSlice[0]
+	fmt.Println("path: " + path)
+	fmt.Println("bucket name: " + bucketName)
+
+	_, err := os.Stat("data/" + bucketName)
+	if err == nil {
+	} else if os.IsNotExist(err) {
+		DisplayError(w, http.StatusInternalServerError, "There is no such directory: ", err)
+		return
+	} else {
+		DisplayError(w, http.StatusNotFound, "Failed to check the directory presence: ", err)
+		return
+	}
+
+	object, err := os.OpenFile("data/"+path, os.O_RDWR|os.O_CREATE, 0o644)
+	if err != nil {
+		DisplayError(w, http.StatusInternalServerError, "Failed to create/open a file", err)
+		return
+	}
+	defer object.Close()
 }
 
 func GetObjects(w http.ResponseWriter, req *http.Request) {
